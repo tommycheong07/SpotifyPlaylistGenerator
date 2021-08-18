8e8bc96fd6b48a8886588b9cc4c544bee960462f
Tommy Cheong <tommycheong98@gmail.com>
2021-08-18 23:27:31 +0800

    added radar chart analysis of new playlist

---
+++ src/build_radar_chart.py
@@ -0,0 +1,22 @@
+import plotly.express as px
+import pandas as pd
+
+def jsonToPD(json):
+    appendDF = pd.DataFrame({"danceability": [json[0]['danceability']],
+                            'energy': [json[0]['energy']],
+                            'acousticness': [json[0]['acousticness']],
+                            'instrumentalness': [json[0]['instrumentalness']],
+                            'liveness': [json[0]['liveness']],
+                            'valence': [json[0]['valence']],
+                            })
+    return appendDF
+
+def createRadar(df):    
+    newPD = pd.DataFrame(dict(
+        r = [df['danceability'].mean(), df['energy'].mean(),
+            df['acousticness'].mean(), df['instrumentalness'].mean(), df['liveness'].mean(),
+            df['valence'].mean()],
+        theta = ['danceability', 'energy', 'acousticness', 'instrumentalness', 'liveness', 'valence']))
+
+    fig = px.line_polar(newPD, r='r', theta='theta', line_close=True)
+    fig.show()
--- src/top_songs_recommender.py
+++ src/top_songs_recommender.py
@@ -1,30 +1,32 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred
+import build_radar_chart
+import pandas as pd
 
 ## Authentication and scope declaration
 scope = "user-top-read,playlist-modify-public,playlist-modify-private,user-read-private,user-read-email"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_ID, client_secret= cred.client_SECRET, redirect_uri=cred.redirect_url, scope=scope))
 
-## Getting playlist information
-print("What would you like your playlist to be called?")
-title = input()
-print("What is the description of the " + title + " playlist?")
-description = input()
-print("Would you like this to be a public or private playlist? public or private")
-public = input()
-if public == 'public':
-    public = True
-else:
-    public = False
+# ## Getting playlist information
+# print("What would you like your playlist to be called?")
+# title = input()
+# print("What is the description of the " + title + " playlist?")
+# description = input()
+# print("Would you like this to be a public or private playlist? public or private")
+# public = input()
+# if public == 'public':
+#     public = True
+# else:
+#     public = False
 
 
 ## Getting user information
 user = sp.me()
 
-## Create playlist and find playlist ID
-new_playlist = sp.user_playlist_create(user=user['id'], name=title, public=public, description=description)
-playlist_id = new_playlist['id']
+# ## Create playlist and find playlist ID
+# new_playlist = sp.user_playlist_create(user=user['id'], name=title, public=public, description=description)
+# playlist_id = new_playlist['id']
 
 
 top_tracks = sp.current_user_top_tracks(limit=20, offset=0, time_range="medium_term")
@@ -48,11 +50,19 @@ genres = ",".join(top3Genres)
 
 # print(genres)
 
+column_names = ['danceability', 'energy', 'acousticness', 'instrumentalness', 'liveness', 'valence']
+df = pd.DataFrame(columns=column_names)
+
+
 for idx, item in enumerate(top_tracks['items']):
     artistID = [item['artists'][0]['id']]
     trackID = [item['id']]
     recs = sp.recommendations(seed_artists=artistID, seed_genres=top3Genres, seed_tracks=trackID, limit=3, country='US')
 
     for idx, r in enumerate(recs['tracks']):
-        sp.user_playlist_add_tracks(user = user['id'], playlist_id = playlist_id, tracks = [r['uri']])
+        # sp.user_playlist_add_tracks(user = user['id'], playlist_id = playlist_id, tracks = [r['uri']])
+        json = sp.audio_features(r['id'])
+        trackDF = build_radar_chart.jsonToPD(json)
+        df = df.append(trackDF)
 
+build_radar_chart.createRadar(df)
